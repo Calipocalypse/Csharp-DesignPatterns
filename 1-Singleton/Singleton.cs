ee943bb0bf9b2a979731ede17a1c205e15e9157d
Calipocalypse <calipocalypse@gmail.com>
2022-06-12 11:30:46 +0200

    1. Singleton

---
+++ 1-Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns._1_Singleton
+{
+    internal class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        /* Singleton code down there */
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns._1_Singleton;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter number which corresponds with design pattern you want to run");
+        string number = Console.ReadLine();
+        Console.Clear();
+        switch (number)
+        {
+            case "1": RunSingleton();
+                break;
+            default: Console.WriteLine("Not found");
+                break;
+        }
+        Console.WriteLine("");
+        Main();
+    }
+
+    private static void RunSingleton()
+    {
+        Singleton singleton1 = Singleton.GetInstance();
+        Singleton singleton2 = Singleton.GetInstance();
+
+        if (singleton1 == singleton2) Console.WriteLine("Singleton runs only one instance");
+        else Console.WriteLine("Something gone wrong with singleton code");
+    }
+}
